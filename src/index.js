a418626aff6edcff9b8b012b9bd4e1dc7d11ed4b
Alphacreed <wynoral@gmail.com>
2023-09-01 03:42:21 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ConfigProvider theme={{
+      components:{
+        Button:{
+          colorPrimary:'#405138',
+          colorPrimaryHover:'#405138',
+          borderRadius:'2px'
+        }
+      },
+      token:{
+        borderRadius:'2px',
+      }
+    }}>
     <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
